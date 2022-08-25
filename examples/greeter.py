59c451eecef91541b6d19763e5b216adb1c7edb8
Daan van der Kallen <mail@daanvdk.com>
2022-08-25 13:42:52 +0200

    Small greeter tweaks

--- examples/greeter.py
+++ examples/greeter.py
@@ -1,18 +1,20 @@
 from vivi import Vivi
 from vivi.elements import component, fragment, h
 from vivi.hooks import use_state, use_callback
+from vivi.events import prevent_default
 
 
 @component
 def greeter():
-    name, set_name = use_state('World')
+    name, set_name = use_state('')
 
     @use_callback(set_name)
+    @prevent_default
     def oninput(e):
         set_name(e.value)
 
     return fragment(
-        h.input(value=name, oninput=oninput),
+        h.input(autofocus=True, value=name, oninput=oninput),
         h.div('Hello, ', name, '!'),
     )
 
